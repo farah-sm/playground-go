354ec02f8e2bda5ae6b263f20295037124691b3b
Saed Farah <saedf@google.com>
2024-10-14 16:13:24 +0000

    Advancements - User Input, Print, Variables. Logic

--- src/new-booking-app/main.go
+++ src/new-booking-app/main.go
@@ -3,17 +3,36 @@ package main
 
 import "fmt"
 
-
 func main() {
-	p, j := 0, 55
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
 
-	// the value of i is the memory location of p
-	i := &p
-	// we change the value of p by reference, using the i variable
-	*i = 22
+	greeting := "Go conference"
+	const confTickets = 50
+	var remainTickets uint = 50
+
+	
+	
+	fmt.Print("What is your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("What is your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("What is your email: ")
+	fmt.Scan(&email)
+
+	fmt.Print("How many tickets would you like: ")
+	fmt.Scan(&userTickets)
+
+	remainTickets = remainTickets - uint(userTickets)
+
+	fmt.Printf("Welcome to %v, we hope you enjoy your time here\n", greeting)
+	fmt.Printf("There are %v in total. There are: %v remaining.\n", confTickets, remainTickets)
 
 
-	// the value of p is now 22 (after line 13), j is still 55
-	// and the value of the i is still the hexadecimal memory location of the variable p
-	fmt.Print(p, j, i)
+	fmt.Printf("Thank you %v %v for buying: %v tickets. You will recieve a confirmation email at: %v. There are now %v tickets remaining\n", firstName, lastName, userTickets, email, remainTickets)
+
 }
