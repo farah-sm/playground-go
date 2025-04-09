2f1a569932d270ef8447945d81d6348b48256d12
saed <65547726+farah-sm@users.noreply.github.com>
2025-04-09 13:55:21 +0100

    Update config-scanner.go

--- src/tools/config-scanner.go
+++ src/tools/config-scanner.go
@@ -1,26 +1,18 @@
 package main
 
-package main
-
-
 import (
-	// "k8s.io/client-go/tools/clientcmd"
-	// "k8s.io/client-go/util/homedir"
 	"fmt"
 	"os"
 	"strings"
-	// "io/fs"
 	"path/filepath"
 	"flag"
 )
 
-
-
 func main() {
 
-	var files []os.FileInfo
+// 	var files []os.FileInfo
 	fileName := flag.String("file", "", "Filename pattern we're looking for, default is kubeconfig")
-	startPath := flag.String("path", "/", "Where shall we scan, default is root")
+	startPath := flag.String("path", ".", "Where shall we scan, default is root")
 	flag.Parse()
 
 	if *fileName == "" {
@@ -30,27 +22,14 @@ func main() {
 	}
 	fmt.Printf("Searching for %s starting from %s...\n", *fileName, *startPath)
 
-	//fmt.Printf("%v", *fileName)
 	err := filepath.Walk(*startPath, func(path string, info os.FileInfo, err error) error {
 		if strings.Contains(info.Name(), *fileName) {
-			files = append(files, info)
+			fmt.Printf("%s \n", path)
 		}
 		return nil
 })
     if err != nil {
         println("Error", err)
-    } else {
-        for _, f := range files {
-            println(f.Name())
-            // This is where we'd like to open the file
-        }
-
-
-}
-
-}
-
-
-}
+    }
 
 }
