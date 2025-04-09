96e9de3d79b0f56720a75d85274d7cd0391c7323
saed <65547726+farah-sm@users.noreply.github.com>
2025-04-09 16:21:48 +0100

    Update config-scanner.go

--- src/tools/config-scanner.go
+++ src/tools/config-scanner.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+    "bufio"
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 	"path/filepath"
-	"flag"
+	"strings"
 )
 
 func main() {
@@ -24,7 +25,58 @@ func main() {
 
 	err := filepath.Walk(*startPath, func(path string, info os.FileInfo, err error) error {
 		if strings.Contains(info.Name(), *fileName) {
-			fmt.Printf("%s \n", path)
+			fmt.Printf("A file with Kubeconfig in its name: %s \n", path)
+		} else {
+			// Confirms n is a regular file and not a directory
+			if info.Mode().IsRegular() {
+				// fmt.Printf("%s is a regular file\n", info.Name())
+				filer, err := os.Open(path)
+				if err != nil {
+					//
+				}
+				defer filer.Close()
+
+/// -------------- OPTION 1
+				// b, err := os.ReadFile(path)
+				// if err != nil {
+				// 	//
+				// }
+				// s := string(b)
+
+				// if strings.Contains(s, "kind: config") {
+				// 	fmt.Printf("File contains 'Kind: Config': %s\n", path)
+				// }
+
+
+
+/// -------------- OPTION 2
+
+			scanner := bufio.NewScanner(filer) // NewScanner uses ScanLines func which returns each line of text, stripped of any trailing end-of-line marker.
+			for scanner.Scan() {
+				line := scanner.Text()
+				configFile := "Kind: Config"
+				if strings.Contains(line, configFile) {
+					fmt.Printf("The path of a file that contains config: %s. The Line that matches: %s\n", path, line)
+				}
+				
+			 }
+			
+
+			 }
+			// 		fmt.Printf("%s\n", info.Name())
+			// }
+			// Open file now & assess file contents for 'kind: config'
+			// filer, err := os.Open(info.Name())
+
+			// if err != nil {
+			// 	println("Error234", err)
+			// }
+			// defer filer.Close()
+
+			//scanner := bufio.NewScanner(filer) // NewScanner uses ScanLines func which returns each line of text, stripped of any trailing end-of-line marker.
+			//fmt.Println(*scanner)
+
+			
 		}
 		return nil
 })
