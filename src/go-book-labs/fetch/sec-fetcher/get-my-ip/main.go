b907c453f92513da3444564a6e5aabb06ffb9cfd
Home <home@Homes-MacBook-Air.local>
2025-04-25 14:47:36 +0300

    New tool, simply get's your current IP address as seen from the Internet.

---
+++ src/go-book-labs/fetch/sec-fetcher/get-my-ip/main.go
@@ -0,0 +1,46 @@
+package main
+// API: https://developer.shodan.io/api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+	var myip string 
+
+func main() {
+	api := os.Getenv("API")
+
+
+
+	// url := fmt.Sprintf("https://api.shodan.io/shodan/host/%s?key=%s", *ip, api)
+
+	url := fmt.Sprintf("https://api.shodan.io/tools/myip?key=%s", api)
+
+	//fmt.Printf("\nURL: %s\n", url)
+
+	resp, err := http.Get(url)
+	if err!=nil {
+		fmt.Printf("Error with getting content from URL: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+			log.Fatalf("Error reading response body: %v", err)
+	}
+
+	// Unmarshal the JSON response into the ports slice
+	err = json.Unmarshal(body, &myip)
+	if err != nil {
+			log.Fatalf("Failed to decode JSON: %v", err)
+	}
+
+	fmt.Println("-------------------------")
+	fmt.Printf("Your public IP: %v\n", myip)
+	fmt.Println("-------------------------")
+}
